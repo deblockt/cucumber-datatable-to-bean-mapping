56d53aa48921d13a4726bbbc2679d9fe487e36f1
Thomas Deblock <deblock.thomas.62@gmail.com>
2022-12-07 17:20:35 +0100

    docs: add README to explain how works the library

--- lib/src/main/java/com/deblock/cucumber/datatable/annotations/DataTableWithHeader.java
+++ lib/src/main/java/com/deblock/cucumber/datatable/annotations/DataTableWithHeader.java
@@ -5,6 +5,27 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation should be used on a java class or record to register is as a datatable compliant bean.
+ *
+ * <pre>
+ *   @DataTableWithHeader
+ *   class Bean {
+ *       @Column("column 1")
+ *       public String column1;
+ *
+ *       @Column(value = "column2", mandatory = false)
+ *       public String column2;
+ *   }
+ *   @DataTableWithHeader
+ *   record Bean(
+ *       @Column("column 1")
+ *       String column1,
+ *       @Column(value = "column2", mandatory = false)
+ *       String column2
+ *   ) { }
+ * </pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.RECORD_COMPONENT})
 public @interface DataTableWithHeader {
